7a1adaebd55cda3e1e00c1b7c508819bea250da2
kezif <kezif6262@gmail.com>
2018-09-28 18:29:04 +0300

    Work w color

---
+++ src/Col.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+
+public class Col {
+
+
+    static Color addFunkyShCol(Color c, double h, double m){
+        int r = (int)(c.getRed() + addMH(m, h));
+        int g = (int)(c.getGreen() + addMH(m, h));
+        int b = (int)(c.getBlue() + addMH(m, h));
+        r = round(r, 0, 255);
+        g = round(g, 0,255);
+        b = round(b, 0, 255);
+        return new Color(r,g,b);
+    }
+
+    static int round(int value ,int bottom, int top){
+        value = value > top ? top : value;
+        value = value < bottom ? bottom : value;
+        return value;
+    }
+
+    static double addMH(double h,double m){
+        return  - m * (255/3);
+    }
+}
--- src/Square.java
+++ src/Square.java
@@ -9,12 +9,10 @@ public class Square {
 
 
 
-
     public Square(){
         System.out.println("f u");
         throw new java.lang.Error("try something else");
     }
-
     public Square(int x, int y, int seed){
         this.x = x;
         this.y = y;
@@ -43,30 +41,8 @@ public class Square {
         this.moist = moist;
     }
 
-    public Color getColor(){
-            switch (bio) {
-                case RIVER:
-                    return Color.decode("#41C4EB");
-                case DESERT:
-                    return Color.decode("#EDC9AF");
-                case PLAIN:
-                    return Color.decode("#49b484");
-                case FOREST:
-                    return Color.decode("#228B22");
-                case BRUSH:
-                    return Color.decode("#7d8e74");
-                case TAIGA:
-                    return Color.decode("#808000");
-                case MOUNT:
-                    return Color.decode("#977c53");
-                case SNOW:
-                    return Color.WHITE;
-                case SITY:
 
-                default:
-                    return Color.getHSBColor((float) height, .7f, .535f);
-            }
-    }
+
 
 
     private void calcHeight(int seed){
@@ -75,7 +51,6 @@ public class Square {
         height = SimplexNoise.sumOctave(16, x, y, seed, pers, scale);
         height = (height+1)/2;
     }
-
     private void calcMoinst (int seed) {
         double scale = .004;
         double pers = .3;
@@ -86,7 +61,7 @@ public class Square {
 
     public void setBio(){
         //TODO биомы нормально
-        if(height < .3f){
+        if(height < .25f){
                 bio = Biome.RIVER;
         }
         else if(height < .5f){
@@ -109,6 +84,39 @@ public class Square {
 
     }
 
+    public Color getColor(){
+        switch (bio) {
+            case RIVER:
+                //return Color.decode("#41C4EB");
+                return Col.addFunkyShCol(Color.decode("#41C4EB"), getHeight(), getMoist());
+            case DESERT:
+                //return Color.decode("#EDC9AF");
+                return Col.addFunkyShCol(Color.decode("#EDC9AF"), getHeight(), getMoist());
+            case PLAIN:
+                //return Color.decode("#49b484");
+                return Col.addFunkyShCol(Color.decode("#49b484"), getHeight(), getMoist());
+            case FOREST:
+                //return Color.decode("#228B22");
+                return Col.addFunkyShCol(Color.decode("#228B22"), getHeight(), getMoist());
+            case BRUSH:
+                //return Color.decode("#7d8e74");
+                return Col.addFunkyShCol(Color.decode("#7d8e74"), getHeight(), getMoist());
+            case TAIGA:
+                //return Color.decode("#808000");
+                return Col.addFunkyShCol(Color.decode("#808000"), getHeight(), getMoist());
+            case MOUNT:
+                //return Color.decode("#977c53");
+                return Col.addFunkyShCol(Color.decode("#977c53"), getHeight(), getMoist());
+            case SNOW:
+                //return Color.WHITE;
+                return Col.addFunkyShCol(Color.WHITE, getHeight(), getMoist());
+            case SITY:
+
+            default:
+                return Color.getHSBColor((float) height, .7f, .535f);
+        }
+    }
+
 
 
     static double averangeHeight (Square[][] s){
@@ -122,7 +130,6 @@ public class Square {
         av /= (numberSq*numberSq);
         return av;
     }
-
     static Biome averangeBiome(Square[][] s){
         int len = s.length;
         Biome[] bio = new Biome[len * len];
@@ -133,6 +140,21 @@ public class Square {
         }
         return Biome.maxBiome(bio);
     }
+
+    static public float distance(Square s1, Square s2){
+        return (float)Math.sqrt(Math.pow(s2.getX() - s1.getX(),2) + Math.pow(s2.getY() - s1.getY(),2));
+    }
+    static public int distanceLinear(Square s1, Square s2){
+        int x = Math.abs(s1.getX() - s2.getX());
+        int y = Math.abs(s1.getY() - s2.getY());
+        int result = y > x ? (14*x + 10*(y-x)) : (14*y + 10*(x-y));
+        return result;
+    }
+    static public float distanceLinearWHeight(Square s1, Square s2){
+        int d = distanceLinear(s1,s2);
+        return  d;
+    }
+
 
 
 }
